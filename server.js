742e368d43629478b677f7e9987b238e57aa37e2
lindsey tummond <lindseytummond@gmail.com>
2020-04-26 13:52:24 -0700

    added server.js dependencies, routes, listening port

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -12,84 +11,17 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
-var reservations = [
-  {
-    id: 123,
-    name: "Lindsey",
-    email: "lindsey@example.com",
-    phone: 123-456-7890,
-  },
-  {
-    id: 456,
-    name: "Sean",
-    email: "sean@example.com",
-    phone: 11-456-7890,
-  },
-  {
-    id: 789,
-    name: "Travis",
-    email: "lindseytummond@gmail.com",
-    phone: 123-456-7890,
-  }
-];
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
-
-app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
-});
-
-app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
-});
-
-// Displays all reservations
-// app.get("/restaurant/reservations", function(req, res) {
-//   return res.json(reservations);
-// });
-
-// // Displays a single character, or returns false
-// app.get("/restaurant/reservations/:reservations", function(req, res) {
-//   var chosen = req.params.reservations;
-
-//   console.log(chosen);
-
-//   for (var i = 0; i < reservations.length; i++) {
-//     if (chosen === reservations[i].routeName) {
-//       return res.json(reservations[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// Create New Characters - takes in JSON input
-// app.post("/restauarant/reservations/new", function(req, res) {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   // This works because of our body parsing middleware
-//   var newreservation = req.body;
-
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
-
-//   console.log(newreservation);
-
-//   characters.push(newreservation);
-
-//   res.json(newreservation);
-// });
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on localhost:"+ PORT);
+  console.log("App listening on http:localhost:"+ PORT);
 });
